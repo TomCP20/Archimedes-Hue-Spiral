1ebf0f08b835e122d1896cc4417440005ab2959f
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-26 18:58:11 +0000

    added header to page

--- src/App.tsx
+++ src/App.tsx
@@ -33,25 +33,28 @@ export default function App() {
   }, [speedText])
 
   return (
-    <div className='parent'>
-      <div className='left'>
-        <label htmlFor="speed">Speed: {speedText}</label><br />
-        <input type="range" id="speed" name="speed" defaultValue="1" min="0" max="10" step="0.01" onChange={e => setSpeedText(+e.target.value)} /><br />
+    <>
+      <h1>Archemidies spiral</h1>
+      <div className='parent'>
+        <div className='left'>
+          <label htmlFor="speed">Speed: {speedText}</label><br />
+          <input type="range" id="speed" name="speed" defaultValue="1" min="0" max="10" step="0.01" onChange={e => setSpeedText(+e.target.value)} /><br />
 
-        <label htmlFor="repetitions">Repetitions: {repetitions}</label><br />
-        <input type="range" id="repetitions" name="repetitions" defaultValue="1" min="0" max="10" step="1" onChange={e => setRepetitions(+e.target.value)} /><br />
+          <label htmlFor="repetitions">Repetitions: {repetitions}</label><br />
+          <input type="range" id="repetitions" name="repetitions" defaultValue="1" min="0" max="10" step="1" onChange={e => setRepetitions(+e.target.value)} /><br />
 
-        <label htmlFor="distance">Distance: {distance}</label><br />
-        <input type="range" id="distance" name="distance" defaultValue="1" min="0" max="20" step="0.01" onChange={e => setDistance(+e.target.value)} /><br />
+          <label htmlFor="distance">Distance: {distance}</label><br />
+          <input type="range" id="distance" name="distance" defaultValue="1" min="0" max="20" step="0.01" onChange={e => setDistance(+e.target.value)} /><br />
+        </div>
+        <div className='center'>
+          <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 20], left: -20, right: 20, bottom: -20, top: 20 }} style={{ background: "black" }} >
+            <ambientLight />
+            <Triangle position={[0, 0, 0]} size={5} spin={0} myShader={myShader} speed={speed} />
+          </Canvas>
+        </div>
+        <div className='right'></div>
       </div>
-      <div className='center'>
-        <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 20], left: -20, right: 20, bottom: -20, top: 20 }} style={{ background: "black" }} >
-          <ambientLight />
-          <Triangle position={[0, 0, 0]} size={5} spin={0} myShader={myShader} speed={speed} />
-        </Canvas>
-      </div>
-      <div className='right'></div>
-    </div>
+    </>
   )
 }
 
