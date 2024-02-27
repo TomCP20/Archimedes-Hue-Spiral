30a817d963da09296d442ac4b55d5648f7fa758a
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-27 16:22:39 +0000

    fixed nested ring

--- src/objects/NestedRing.tsx
+++ src/objects/NestedRing.tsx
@@ -1,13 +1,13 @@
 import { ShaderMaterial } from 'three';
 import Ring from './Ring';
 
-export default function NestedRing(props: Readonly<{myShader:  React.RefObject<ShaderMaterial>; speed:  React.MutableRefObject<number>}>) {
-  const {myShader, speed} = props;
+export default function NestedRing(props: Readonly<{myShader:  React.RefObject<ShaderMaterial>; speedRef:  React.MutableRefObject<number>}>) {
+  const {myShader, speedRef} = props;
   return (
     <>
-      <Ring items={18} radius={3} size={0.8} spin={-1} myShader={myShader} speed={speed} />
-      <Ring items={12} radius={2} size={0.8} spin={1} myShader={myShader} speed={speed} />
-      <Ring items={6} radius={1} size={0.8} spin={-1} myShader={myShader} speed={speed} />
+      <Ring items={18} radius={3} size={0.8} spin={-1} myShader={myShader} speedRef={speedRef} />
+      <Ring items={12} radius={2} size={0.8} spin={1} myShader={myShader} speedRef={speedRef} />
+      <Ring items={6} radius={1} size={0.8} spin={-1} myShader={myShader} speedRef={speedRef} />
     </>
   );
 }
