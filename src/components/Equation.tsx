922d99e92f8bdf26849a30c4ebbe4c6ff9def4e6
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-27 17:14:33 +0000

    improved equation definitions.

--- src/components/Equation.tsx
+++ src/components/Equation.tsx
@@ -5,22 +5,28 @@ export function Equation() {
     <h3>Definitions</h3>
     <dl>
       <dt>hue</dt>
-      <dd>The hue of the pixel</dd>
+      <dd>The hue of the pixel.</dd>
 
       <dt>time</dt>
-      <dd>Every frame time is incrimented by delta*speed</dd>
+      <dd>The time since the scene started, every frame time is incrimented by delta*speed.</dd>
+
+      <dt>delta</dt>
+      <dd>The amount of time since the last frame.</dd>
+
+      <dt>speed</dt>
+      <dd>the rate the time changes.</dd>
 
       <dt>angle</dt>
-      <dd>The angle of the pixels position from the negative x axis</dd>
+      <dd>The angle of the pixels position from the negative x axis.</dd>
 
       <dt>repetitions</dt>
-      <dd>The number of times the hue gradient repeats</dd>
+      <dd>The number of times the hue gradient repeats.</dd>
 
       <dt>radius</dt>
-      <dd>The distance of the pixel from the center</dd>
+      <dd>The distance of the pixel from the center.</dd>
 
       <dt>distance</dt>
-      <dd>The inverse of distance between the spiral arms</dd>
+      <dd>The inverse of distance between the spiral arms.</dd>
     </dl>
   </div>;
 }
