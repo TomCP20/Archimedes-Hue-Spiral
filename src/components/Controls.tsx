f3cc694cec88b6f399c9921bcaa477e2c23f2295
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-28 18:23:26 +0000

    refactored controls
    now each slider is reset independently

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import Slider from './Slider';
 import { Settings } from '../interfaces/settings';
 
@@ -10,32 +9,12 @@ export default function Controls(props: Readonly<ControlsProps>) {
 
   const { settings } = props;
 
-  const [speed, setSpeed] = useState(1);
-
-  useEffect(() => {
-    settings.current.speed = speed;
-  }, [settings, speed]);
-
-  const [repetitions, setRepetitions] = useState(1);
-
-  useEffect(() => {
-    settings.current.repetitions = repetitions;
-  }, [settings, repetitions]);
-
-  const [distance, setDistance] = useState(1);
-
-  useEffect(() => {
-    settings.current.distance = distance;
-  }, [settings, distance]);
-
   return (
-
     <div className="controls panel">
       <h2>Controls</h2>
-      <Slider name={"speed"} min={-10} max={10} step={0.01} ticks={[-10, -5, -1, 0, 1, 5, 10]} val={speed} setVal={setSpeed} />
-      <Slider name={"repetitions"} min={0} max={10} step={1} ticks={[0, 1, 10]} val={repetitions} setVal={setRepetitions} />
-      <Slider name={"distance"} min={-20} max={20} step={0.01} ticks={[-20, -1, 0, 1, 20]} val={distance} setVal={setDistance} />
-      <button onClick={() => { setSpeed(1); setRepetitions(1); setDistance(1); }}>Reset</button>
+      <Slider name={"speed"} min={-10} max={10} step={0.01} ticks={[-10, -5, -1, 0, 1, 5, 10]} settings={settings} />
+      <Slider name={"repetitions"} min={0} max={10} step={1} ticks={[0, 1, 10]} settings={settings} />
+      <Slider name={"distance"} min={-20} max={20} step={0.01} ticks={[-20, -1, 0, 1, 20]} settings={settings} />
     </div>
   );
 }
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,23 +1,32 @@
+import { useEffect, useState } from "react";
+import { Settings } from "../interfaces/settings";
+
 interface SliderProps {
-  name: string;
+  name: keyof Settings;
   min: number;
   max: number;
   step: number;
   ticks: number[];
-  val: number;
-  setVal: React.Dispatch<React.SetStateAction<number>>;
+  settings: React.MutableRefObject<Settings>;
 }
 
 export default function Slider(props: Readonly<SliderProps>) {
-  const { name, min, max, step, ticks, val, setVal } = props;
+  const { name, min, max, step, ticks, settings } = props;
+
+  const [val, setVal] = useState(1);
+
+  useEffect(() => {
+    settings.current[name] = val;
+  }, [name, settings, val]);
+
   return (
     <p>
       <label htmlFor={name}>{name[0].toUpperCase() + name.slice(1)}: {val}</label>
       <br />
-      <input type="range" id={name} name={name} list={name+"list"} defaultValue="1" value={val} min={min} max={max} step={step} onChange={e => setVal(+e.target.value)} />
-      <datalist id={name+"list"}>
-        {ticks.map(tick => <option key={tick} value={tick}></option>)}
-      </datalist>
+      <input type="range" id={name} name={name} list={name + "list"} value={val} min={min} max={max} step={step} onChange={e => setVal(+e.target.value)} />
+      <datalist id={name + "list"}>{ticks.map(tick => <option key={tick} value={tick}></option>)}</datalist>
+      <br />
+      <button onClick={() => { setVal(1); }}>Reset</button>
     </p>
   );
 }
